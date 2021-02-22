aa1ce5dbcf3ef45156dc290a7c9f26697a552a22
Andreiev_Sergeii <sergey.andreyev@sgroshi.com>
2021-02-22 15:27:35 +0300

    new

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -39,26 +39,19 @@ export default function Posts({ posts }) {
     </div>
   );
 }
-// Posts.getInitialProps = async (ctx) => {
-//   console.log("ctx", ctx.pathname);
-//   const res = await getAllPosts();
-//   console.log("res", res);
-
-//   return { posts: res.posts };
-// };
-
-// pathname - Current route. That is the path of the page in /pages
-// query - Query string section of URL parsed as an object
-// asPath - String of the actual path (including the query) shown in the browser
-// req - HTTP request object (server only)
-// res - HTTP response object (server only)
-// err - Error object if any error is encountered during the rendering
-
-export async function getServerSideProps(context) {
+Posts.getInitialProps = async (ctx) => {
+  console.log("ctx", ctx.pathname);
   const res = await getAllPosts();
-  const posts = res.posts;
-  return {
-    props: { posts },
-  };
-}
-//вызывается только на сервере и позволяет работать с БД напрямую
+  console.log("res", res);
+
+  return { posts: res.posts };
+};
+
+// export async function getServerSideProps(context) {
+//   const res = await getAllPosts();
+//   const posts = res.posts;
+//   return {
+//     props: { posts },
+//   };
+// }
+// //вызывается только на сервере и позволяет работать с БД напрямую
